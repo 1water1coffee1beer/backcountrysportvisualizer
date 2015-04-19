1a6e3067b80d359c37cc316bbfe730ea6741aa5c
Brian Sweeney <briangsweeney@gmail.com>
2015-04-19 11:38:02 -0400

    got scrolling to be reasonable

--- BackcountrySportVisualizer/MainWindow.xaml.cs
+++ BackcountrySportVisualizer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
 
 namespace BackcountrySportVisualizer
 {
@@ -23,6 +26,44 @@ namespace BackcountrySportVisualizer
         public MainWindow()
         {
             InitializeComponent();
+
+            this.mainWindow.WindowState = WindowState.Maximized;
+
+            this.map.MapProvider = GMapProviders.BingHybridMap;
+            this.map.Manager.Mode = AccessMode.ServerAndCache;
+
+            this.map.Position = new PointLatLng(39, -105.547222);
+
+            this.map.MouseWheel += map_MouseWheel;
+            this.topoRadio.Checked += topoRadio_Checked;
+            this.bingRadio.Checked += bingRadio_Checked;
+        }
+
+        void bingRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            this.map.MapProvider = GMapProviders.BingHybridMap;
+            this.topoRadio.IsChecked = false;
+        }
+
+        void topoRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            this.map.MapProvider = GMapProviders.ArcGIS_Topo_US_2D_Map;
+            this.bingRadio.IsChecked = false;
+        }
+
+        void map_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            int delta = e.Delta > 0 ? 1 : -1;
+
+            int currentZoom = (int)this.map.Zoom;
+            
+            int maxzoom = this.map.MaxZoom;
+            int minzoom = this.map.MinZoom;
+
+            if (currentZoom + delta >= minzoom && currentZoom <= maxzoom)
+            {
+                this.map.Zoom = currentZoom + delta;
+            }
         }
     }
 }
